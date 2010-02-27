59c507b9ac33f08487eaa207830d4d96e882e3b3
Hook Bot 3000 <marak.squires@gmail.com>
2010-02-27 12:06:28 +0000

    added missing include for module

--- hookio/jsonrpc.js
+++ hookio/jsonrpc.js
@@ -1,4 +1,7 @@
 // hook.io json-rpc
+
+var hookIO = require('./index').hookIO;
+
 hookIO.addListener('JsonrpcRequest', function(request, response) {
   response.writeHeaders(200, {});
   response.write('JSONRPC');
