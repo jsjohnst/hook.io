a55071c0f47eabf6f64c53402daabc7e41613b57
Hook Bot 3000 <marak.squires@gmail.com>
2010-02-27 12:08:32 +0000

    i maded an event, /jsonrpc now routes to correct event

--- hookio/jsonrpc.js
+++ hookio/jsonrpc.js
@@ -1,9 +1,8 @@
 // hook.io json-rpc
-
 var hookIO = require('./index').hookIO;
 
 hookIO.addListener('JsonrpcRequest', function(request, response) {
-  response.writeHeaders(200, {});
+  response.sendHeader(200, {});
   response.write('JSONRPC');
   response.close();
 });
