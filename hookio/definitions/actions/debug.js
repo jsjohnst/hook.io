d84a70bc67bfe61f1b79f276e04c8d78e40b35d1
Marak <marak.squires@gmail.com>
2010-03-27 01:29:48 -0400

    added stubs for send_email, debug, twitter_update_status

--- hookio/definitions/actions/debug.js
+++ hookio/definitions/actions/debug.js
@@ -10,43 +10,22 @@ exports.action = {
   config: {
 			  'payload': {
       label: 'dump of triggered hook',
-      type: 'object',
+      type: 'text',
       description: 'stub action will output the payload of the hook',
-      validate: function() {
-        // TODO: Validate URL
-        return true;
+      validate: function(input) {
+        // TODO: Validate debug info
+        return input;
       }
     }
   },
   handle: function(action, hook, definition) {
     // Modify hook as needed etc
     var params = hook.get('params');
-
-    if ('string' !== typeof params.contentType)
-      params.contentType = 'application/json';
-
-    if ('http' !== definition.protocol) {
-      // We didn't come from a HTTP hook
-      // Convert time
-      var ret = {
-        'hook': hook.toObject(),
-        'id': action.id
-      };
-
-      // The way this action deals with protocols
-      switch (definition.protocol) {
-        default:
-          ret.result = '';
-          break;
-      }
-
-      params.body = JSON.stringify(ret);
-      ret = null;
-    }
+    params.contentType = 'text/html';
 
     action.set('params', {
       contentType: params.contentType,
-      body: params.body
+      body: 'test body'
     });
   }
 };
---
+++ hookio/definitions/actions/twitter_update_status.js
@@ -0,0 +1,41 @@
+/*
+ * A action for sending e-mails
+ */
+
+exports.action = {
+  name: 'twitter_update_status',
+  title: 'Updates a user\'s twitter status',
+  protocol: 'twitter',
+  keyField: 'username',
+  config: {
+    'username': {
+      label: 'Username',
+      type: 'text',
+      description: 'The username of the twitter account that will login and update status',
+      validate: function(input) {
+        // TODO: Validate e-mail address
+        return input;
+      }
+    },
+    'password': {
+      label: 'Password',
+      type: 'text',
+      description: 'The password of the twitter account that will be logging in',
+      validate: function(input) {
+        // TODO: Validate e-mail address
+        return input;
+      }
+    },
+    'status': {
+      label: 'Status',
+      type: 'text',
+      description: 'The twitter status update',
+      validate: function(input) {
+        // TODO: Validate e-mail address
+        return input;
+      }
+    }
+  },
+  handle: function(action, hook, definition) {
+  }
+};
