87bd3134d413be3329b3616dc06b1d9b7e81dc47
Marak Squires <marak.squires@gmail.com>
2010-04-12 20:49:24 -0400

    pulling some errant commits from production server, api updates

--- hookio/api/core.js
+++ hookio/api/core.js
@@ -3,13 +3,38 @@
  * core API logic for working with the hook.io api
  */
 
-var hookIO = require('../../hookio').hookIO;
+var hookIO = require('../../hookio').hookIO,
+debug = require('../lib/node_debug/node_debug/debug');
 
 exports.ping = function() {
   var callback = arguments[arguments.length - 1];
-  hookIO.debug('hookIO.pong');
+
   try {
     callback(null, 'pong');
+  } catch (error) {
+    callback(error, null);
+  }
+};
+
+exports.getProtocols = function() {
+  var callback = arguments[arguments.length - 1];
+
+  try {
+    callback(null, JSON.stringify(hookIO.protocol));
+  } catch (error) {
+    callback(error, null);
+  }
+};
+
+
+exports.getEvents = function() {
+  var callback = arguments[arguments.length - 1];
+
+	 debug.log(hookIO.hooker);
+
+  try {
+			 //debug.log(hookIO);
+    callback(null, hookIO);
   } catch (error) {
     callback(error, null);
   }
@@ -19,7 +44,7 @@ exports.getDefinitions = function() {
   var callback = arguments[arguments.length - 1];
 
   try {
-    callback(null, hookIO.actioner);
+    callback(null, JSON.stringify(hookIO.protocol));
   } catch (error) {
     callback(error, null);
   }
