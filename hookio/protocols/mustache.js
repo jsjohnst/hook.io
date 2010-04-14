8dfb739e2daee0515f05cc3228dcde27227a01d0
Marak Squires <marak.squires@gmail.com>
2010-04-13 23:05:26 -0400

    fixed mustache protocol based on RayMorgan's advice

--- hookio/protocols/mustache.js
+++ hookio/protocols/mustache.js
@@ -1,4 +1,3 @@
-
 /*
  * hookio/protocols/mustache
  * Implements a mustache protocol for parsing mustache tags out of text
@@ -9,12 +8,14 @@ mu = require('../lib/mu/lib/mu');
 
 exports.start = function() {
   // little test case
-  var renderedText = parse('hello this is a template named {tName}',{"tName":"updog"});
-  hookIO.debug(renderedText);
+  var stream = parse('hello this is a template named {{tName}}',{"tName":"updog"});
+  stream.addListener('data', function (data) {
+    hookIO.debug(data);
+  });
 };
 
 // parse() will perform a sync render with no stream. it is not advised to use this for large blocks of text
 // or operations that run outside of startup
 var parse = exports.parse = function(textWithMustaches, dataToMerge){
-  return mu.compileText(textWithMustaches, dataToMerge);
+  return mu.compileText(textWithMustaches, {})(dataToMerge);
 };
