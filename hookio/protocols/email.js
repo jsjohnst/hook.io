c29242af62d5e171bc58fe6aab34f7e6b9222db0
Marak Squires <marak.squires@gmail.com>
2010-04-26 23:10:12 -0400

    major rename is major

--- hookio/protocols/email.js
+++
@@ -1,9 +0,0 @@
-
-// The email protocol
-
-var hookIO = require('../../hookio').hookIO,
-    mailer = require('../lib/mailer');
-
-exports.send = function send() {
-  mailer.send.apply(mailer, arguments);
-};
